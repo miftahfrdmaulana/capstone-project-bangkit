18d06d39558b1ab9012a40561cd215a93e3a88f9
Miftah Farid Maulana <miftahfrdmaulana@gmail.com>
2024-06-05 19:44:04 +0700

    Add files via upload

---
+++ Cloud Computing/src/services/db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2/promise');
+const { dbConfig } = require('./db-config');
+
+const pool = mysql.createPool(dbConfig);
+
+const query = async (sql, params) => {
+    const [results] = await pool.query(sql, params);
+    console.log('Query results:', results); // Add this line to log the query results
+    return results;
+};
+
+module.exports = { query };
